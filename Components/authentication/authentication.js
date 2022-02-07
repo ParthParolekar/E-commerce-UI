2fe494fd214fa3dbf48a615b961f312abb37f6f6
ParthParolekar <parolekarparth@gmail.com>
2022-02-07 19:20:44 +0530

    refactor: removed console.logs

--- Components/authentication/authentication.js
+++ Components/authentication/authentication.js
@@ -3,11 +3,9 @@ const btnToSignUp = document.querySelector("#btn-to-signup");
 const btnToLogIn = document.querySelector("#btn-to-login");
 
 btnToSignUp.addEventListener("click", () => {
-  console.log("cliked");
   authComponent.classList.add("auth-component-rotate");
 });
 
 btnToLogIn.addEventListener("click", () => {
-  console.log("clicked");
   authComponent.classList.remove("auth-component-rotate");
 });
